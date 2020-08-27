a956cb8332d97eef43bc8acde12ca767be310252
KAUSHAL VASHISTH <backspacekaushal@gmail.com>
2020-08-27 16:34:36 +0530

    Updates

---
+++ precache-manifest.4c018328cf12f9d854f1f0703467c33b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65ecaca11c873e36ee8a2d9bf74960ff",
+    "url": "/Corona-Tracker-301/index.html"
+  },
+  {
+    "revision": "0454ec7325d490571a96",
+    "url": "/Corona-Tracker-301/static/css/main.429b19de.chunk.css"
+  },
+  {
+    "revision": "dbac85200b8d790fbcbc",
+    "url": "/Corona-Tracker-301/static/js/2.9d1ad4ff.chunk.js"
+  },
+  {
+    "revision": "cdea67dd6b4f48205e1e7f8a739e933c",
+    "url": "/Corona-Tracker-301/static/js/2.9d1ad4ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0454ec7325d490571a96",
+    "url": "/Corona-Tracker-301/static/js/main.4d145d25.chunk.js"
+  },
+  {
+    "revision": "29aa27aaf7dc5c99a311",
+    "url": "/Corona-Tracker-301/static/js/runtime-main.012082b5.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Corona-Tracker-301/static/media/image2.d7265326.png"
+  }
+]);
